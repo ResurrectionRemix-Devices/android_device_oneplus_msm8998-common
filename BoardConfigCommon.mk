e771337dc37052394a16dd801dde9d7ac293fc54
maxwen <max.weninger@gmail.com>
2018-01-22 10:07:13 -0500

    Trick to make WiFi load on boot
    Change-Id: I508cf23c6424da377b77c7c0c2485141a03bd7ec
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -286,6 +286,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_LOAD_DELAY := true
 
 
 # Enable real time lockscreen charging current values
