bfaad2a1983b4288a1d9032f18c579a242c60ede
Varun Date <date.varun123@gmail.com>
2018-02-06 04:14:12 +1100

    msm8998-common: build in permissive
    Change-Id: Ib2a90aff85765e8c512d31f9652a41fe077a0b71
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -75,6 +75,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
 TARGET_KERNEL_CONFIG := lineage_oneplus5_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
