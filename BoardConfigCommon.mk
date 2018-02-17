e76a0ba6803c25afd34a9cf85cdc18acaba32d1d
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-02-17 21:11:27 +0100

    msm8998: wifi: wait for kernel driver to get ready
    Depends: https://review.lineageos.org/206958
    
    Change-Id: Ieced27fb0bb5cba6d9850de594b5d4df7977991d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -285,6 +285,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
 
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
