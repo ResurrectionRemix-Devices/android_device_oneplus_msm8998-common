c6d1faf758635999b3edc06a9ca0b26e3ca3e77d
itsjoeoui <maximeyxw@gmail.com>
2020-02-13 23:08:02 -0500

    msm8998-common: I'm very smart I know
    Signed-off-by: itsjoeoui <maximeyxw@gmail.com>

--- common.mk
+++ common.mk
@@ -84,10 +84,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
-		$(LOCAL_PATH)/prebuilts/system/etc/com.google.android.GoogleCamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.GoogleCamera.xml \
-		$(LOCAL_PATH)/prebuilts/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml
-
-
+    $(LOCAL_PATH)/prebuilts/system/etc/permissions/com.google.android.GoogleCamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.GoogleCamera.xml \
+    $(LOCAL_PATH)/prebuilts/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml
 
 # Haters gonna hate..
 PRODUCT_CHARACTERISTICS := nosdcard
@@ -176,7 +174,7 @@ PRODUCT_PACKAGES += \
 # Dex
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
-		NexusLauncherRelease
+    NexusLauncherRelease
 
 # Display
 PRODUCT_PACKAGES += \
