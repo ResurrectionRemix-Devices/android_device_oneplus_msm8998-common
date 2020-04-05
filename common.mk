f333bc55d56c45efbacda3bd20c2bd888c396ef4
itsjoeoui <maximeyxw@gmail.com>
2020-04-05 18:49:01 -0400

    msm8998-common: Nuke ANT+
    * It's 2020
    * Blame Dyne
    
    Signed-off-by: itsjoeoui <maximeyxw@gmail.com>

--- common.mk
+++ common.mk
@@ -119,11 +119,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor \
