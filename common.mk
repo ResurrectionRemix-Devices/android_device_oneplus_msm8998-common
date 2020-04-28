f03b7450a5dcc09f4b660b2d279b225c71e7cead
itsjoeoui <maximeyxw@gmail.com>
2020-04-28 08:47:37 -0400

    Revert "msm8998-common: Shim for camera"
    This reverts commit d809e554b524a367ef71a13e05fb11a5f4497a41.

--- common.mk
+++ common.mk
@@ -134,8 +134,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    libshim_camera 
+    camera.device@3.2-impl
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0
