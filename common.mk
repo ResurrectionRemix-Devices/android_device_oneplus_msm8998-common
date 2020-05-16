44bc9836a943b4f511b3dd525eb83cdb12613330
itsjoeoui <maximeyxw@gmail.com>
2020-05-16 14:27:53 -0400

    msm8998-common: Add back touch HAL
    Signed-off-by: itsjoeoui <idkwhoiam@itsjoeoui.com>

--- common.mk
+++ common.mk
@@ -436,6 +436,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.msm8998
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.oneplus_msm8998
+
 # Tracing
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service.pixel
