79faf1ec310339606be3366439262f4cd8c43521
itsjoeoui <maximeyxw@gmail.com>
2020-05-24 08:56:40 -0400

    msm8998-common: Restore to the old way of handling build fingerprint and description
    Signed-off-by: itsjoeoui <maximeyxw@gmail.com>

--- init/init_oneplus_msm8998.cpp
+++ init/init_oneplus_msm8998.cpp
@@ -73,13 +73,6 @@ void property_override(const std::string& name, const std::string& value)
     }
 }
 
-void property_override_dual(char const system_prop[], char const vendor_prop[],
-    char const value[])
-{
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
-}
-
 void init_target_properties()
 {
     std::string device;
@@ -226,21 +219,4 @@ void vendor_load_properties() {
     init_fingerprint_properties();
     init_alarm_boot_properties();
     init_dalvik_vm_properties();
-
-    std::string device;
-    // fingerprint
-    if (ReadFileToString(DEVINFO_FILE, &device)) {
-        if (!strncmp(device.c_str(), "16859", 5)) {
-            // OnePlus 5
-            property_override("ro.build.description", "OnePlus5-user 8.1.0 OPM1.171019.011 1 release-keys");
-        }
-        else if (!strncmp(device.c_str(), "17801", 5)) {
-            // OnePlus 5T
-            property_override("ro.build.description", "OnePlus5T-user 8.1.0 OPM1.171019.011 1 release-keys");
-        }
-        else {
-            LOG(ERROR) << __func__ << ": unexcepted device!";
-        }
-    }
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys");
 }
