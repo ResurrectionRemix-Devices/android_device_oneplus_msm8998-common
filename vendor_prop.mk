bfd6efb18e3d1c274498cf9120bcaa881f91d348
kerneltoast <sultan@kerneltoast.com>
2020-05-31 09:02:00 -0400

    msm8998-common: Force triple frame buffers
    This yields significantly reduced jitter and doesn't have any measurable impact on latency.
    
    Signed-off-by: itsjoeoui <idkwhoiam@itsjoeoui.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -329,11 +329,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
-    debug.sf.early_phase_offset_ns=1500000 \
-    debug.sf.early_app_phase_offset_ns=1500000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000 \
     debug.sf.enable_gl_backpressure=1
+
+# Force triple frame buffers
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # Timeservice
 PRODUCT_PROPERTY_OVERRIDES += \
