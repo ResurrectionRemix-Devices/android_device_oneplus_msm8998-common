2e9e7eeafd52068ed9d87fd25965f2b8e15b0cf4
karthik990 <knag88@gmail.com>
2020-09-23 07:29:34 +0000

    msm8998-common: Nuke vndk

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -357,9 +357,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     sys.usb.rndis.func.name=gsi \
     sys.usb.rmnet.func.name=gsi
 
-# VNDK
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.vndk.version=28
 
 # VoLTE
 PRODUCT_PROPERTY_OVERRIDES += \
