bfbb4d6d3a420c20461e41ab7319fc15170e7e75
karthik990 <knag88@gmail.com>
2020-09-24 04:23:42 +0000

    msm8998-common: Add support for snap cam

--- common.mk
+++ common.mk
@@ -135,10 +135,12 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
-    camera.device@3.2-impl
+    camera.device@3.2-impl \
+    Snap
 
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
